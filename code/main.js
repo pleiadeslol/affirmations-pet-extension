968606c93839df9318a9f13be82dd8d5b6261188
pleiadeslol <115590428+pleiadeslol@users.noreply.github.com>
2023-03-02 21:46:39 +0100

    changes

---
+++ code/main.js
@@ -0,0 +1,8 @@
+function clickme(){
+    var x = document.getElementById("affirmation");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
